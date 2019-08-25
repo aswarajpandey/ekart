6c0f71eb67812c36e40813feb79b10b126697ba9
aswaraj <aswaraj@aswaraj>
2019-08-25 21:39:08 +0530

    initial commit.

---
+++ ekart/src/main/java/com/example/demo/EkartApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartApplication.class, args);
+		System.out.println("Ekart App Started..");
+	}
+
+}
