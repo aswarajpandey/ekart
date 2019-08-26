bec1893dd9f694373f54801e4416112041678333
aswaraj <aswaraj@aswaraj>
2019-08-26 19:55:57 +0530

    Security code commenting.

--- ekart/src/main/java/com/aswaraj/config/SecurityConfig.java
+++ ekart/src/main/java/com/aswaraj/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * @author aswaraj
+ * Disables Default Security Configuration.
+ *
+ */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
